31fa46dae323a3cc657e1c66da941dc4eb246b41
Mohammed.Nazeer <adilmsrit@gmail.com>
2018-09-01 17:17:05 +0400

    Removed unused file

---
+++ src/main/java/Practice/FinalizeConcept.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class FinalizeConcept {
+    public static void main(String[] args){
+
+        FinalizeConcept f1 = new FinalizeConcept();
+        FinalizeConcept f2 = new FinalizeConcept();
+
+
+
+    }
+}
