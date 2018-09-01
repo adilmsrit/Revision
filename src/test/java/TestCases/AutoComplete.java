0b9adb74f0537a5648dd2c469b5d67e89e2216ea
Mohammed.Nazeer <adilmsrit@gmail.com>
2018-09-01 08:20:34 +0400

    Right Click and double click.

---
+++ src/test/java/TestCases/AutoComplete.java
@@ -0,0 +1,45 @@
+package TestCases;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class AutoComplete {
+    private WebDriver driver;
+    private String baseUrl;
+
+    @BeforeClass
+    public void setup() throws Exception{
+        System.setProperty("webdriver.gecko.driver", "D:\\Selenium\\SeleniumJars\\Firefox and Gecko\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        baseUrl = "https://www.southwest.com/";
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testAutoComplete() throws Exception{
+        driver.get(baseUrl);
+        String searchingText = "New York/Newark, NJ - EWR";
+        String partialText = "New York";
+
+        driver.findElement(By.id("air-city-departure")).sendKeys(partialText);
+
+        //air-city-departure-menu
+        WebElement element = driver.findElement(By.id("air-city-departure-menu"));
+
+
+
+
+
+
+    }
+
+}
